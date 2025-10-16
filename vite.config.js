d75dce27777149b8f2c662d703fd9c7a39c60725
Fernando00188 <Fernnado5565@gmail.com>
2025-10-16 19:52:23 -0400

    Fix Vite base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/minesweeper/', // ✅ needed for GitHub Pages
+  base: './', // ✅ works for Netlify
 })
+
